08f97aba79cf9b0eff410c4a7dd43d4a4ed09318
mongalless <hitstalker7@gmail.com>
2021-01-18 05:10:22 -0500

    Implemented Array class

--- task_2/array.rb
+++ task_2/array.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 class Array
+  def rebuild!
+    map! do |element|
+      case element
+      when Integer
+        yield(element, added_number = size)
+      when Array
+        element.map! do |element_value|
+          yield(element_value, added_number = element.size)
+        end
+      end
+    end
+  end
 end
