1bea44ccfcbf00f7214ae5b29629cbe9643d6351
mongalless <hitstalker7@gmail.com>
2021-01-18 04:37:50 -0500

    Implemented Truncate module

--- task_1/truncate.rb
+++ task_1/truncate.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module Truncate
+  def truncate(attribute, length_hash)
+    attr = attribute.to_s
+    length = length_hash[:length]
+    define_method(attr) do
+      instance_variable_get("@#{attr}".to_sym).chomp[0...length].concat('...')
+    end
+  end
 end
